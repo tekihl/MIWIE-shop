c6cebaee7d524a12a873ac362a903d8de06a36f1
Ibbson <ibbe30@gmail.com>
2025-12-04 12:55:30 +0100

    Skapat landningssida

---
+++ src/vite-env.d.ts
@@ -0,0 +1,13 @@
+/// <reference types="vite/client" />
+
+declare module '*.scss' {
+  const content: { [className: string]: string }
+  export default content
+}
+
+declare module '*.css' {
+  const content: { [className: string]: string }
+  export default content
+}
+
+
