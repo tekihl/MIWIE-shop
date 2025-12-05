574a0a353254df28e3a4a4a34aba2db599fe2867
Martina Lindgren <lindgren.martina@outlook.com>
2025-12-05 15:06:15 +0100

    added code for product details

--- src/ts/products-feed.ts
+++ src/ts/products-feed.ts
@@ -1,42 +1,43 @@
 import { products } from "./product";
+import "./../styles/main.scss";
 
 export const createHtml = () => {
+  const productsContainer = document.getElementById("products-container");
 
-    const productsContainer = document.getElementById("products-container");
+  if (productsContainer) {
+    products.forEach((product) => {
+      const cardWrap = document.createElement("div");
+      const card = document.createElement("div");
+      const cardBody = document.createElement("div");
+      const imgWrap = document.createElement("div");
+      const productTitle = document.createElement("h5");
+      const productPrice = document.createElement("span");
 
-    if (productsContainer) {
+      imgWrap.style.backgroundImage = `url(${product.imageUrl})`;
+      productTitle.innerHTML = product.title;
+      productPrice.innerHTML = String(product.price) + " SEK";
 
-        products.forEach((product) => {
+      cardWrap.classList.add("p-3");
+      cardBody.classList.add("card-body");
+      card.classList.add("border", "col", "card", "bg-light");
+      imgWrap.classList.add("imgWrap");
 
-            const cardWrap = document.createElement('div');
-            const card = document.createElement("div");
-            const cardBody = document.createElement('div');
-            const imgWrap = document.createElement('div');
-            const productTitle = document.createElement("h5");
-            const productPrice = document.createElement("span");
+      card.appendChild(imgWrap);
+      cardBody.appendChild(productTitle);
+      cardBody.appendChild(productPrice);
+      card.appendChild(cardBody);
+      cardWrap.appendChild(card);
+      productsContainer.appendChild(cardWrap);
 
-            imgWrap.style.backgroundImage = `url(${product.imageUrl})`;
-            productTitle.innerHTML = product.title;
-            productPrice.innerHTML = String(product.price) + " SEK";
+      card.dataset.id = product.id.toString();
 
-            cardWrap.classList.add('p-3');
-            cardBody.classList.add('card-body',);
-            card.classList.add('border', 'col', 'card', 'bg-light');
-            imgWrap.classList.add('imgWrap');
+      cardWrap.addEventListener("click", () => {
+        window.location.assign(
+          "http://localhost:5175/product-details.html?id=" + product.id
+        );
+      });
+    });
+  }
+};
 
-            card.appendChild(imgWrap);
-            cardBody.appendChild(productTitle);
-            cardBody.appendChild(productPrice);
-            card.appendChild(cardBody);
-            cardWrap.appendChild(card);
-            productsContainer.appendChild(cardWrap);
-
-            card.dataset.id = product.id.toString();
-
-            cardWrap.addEventListener("click", () => {
-                window.location.assign("http://localhost:5175/product-details.html?id=" + product.id);
-            });
-
-        });
-    }
-}
+createHtml();
