8b8d6747c8fcb274f94a79b1821d80b2e3760dea
“Tom. <tom.kihlborg@gmail.com>
2025-12-09 13:33:28 +0100

    added add to cart  button to details page

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import './styles/main.scss'
-import { createHtml } from './ts/products-feed'
+//import { createHtml } from './ts/products-feed'
 import { getCart } from './ts/cart';
 import { addToCart } from './ts/cart';
 import { removeFromCart } from './ts/cart';
 
-createHtml();
+//createHtml();
 getCart();
 
 document.getElementById("mock-add")?.addEventListener("click", () => {
